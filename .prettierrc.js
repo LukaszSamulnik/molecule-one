b80bcef7b6718cfae6f897207a1df0710c534421
Lukasz Samulnik <samulnik.lukasz@gmail.com>
2022-11-20 16:19:20 +0100

    fixed linting

---
+++ .prettierrc.js
@@ -0,0 +1,12 @@
+export default {
+	semi: true,
+	trailingComma: 'all',
+	singleQuote: true,
+	printWidth: 80,
+	tabWidth: 4,
+	useTabs: true,
+	endOfLine: 'lf',
+	arrowParens: 'avoid',
+	singleAttributePerLine: true,
+	jsxSingleQuote: true,
+};
